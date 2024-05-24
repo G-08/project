7ce917794667c35663d98c6aa51f7112cf843466
Leffy02 <pietro.gasparini@studenti.unitn.it>
2024-05-24 23:40:53 +0200

    provato a fixare api del profilo :(

--- codes/src/app/api/auth/getUserData/route.ts
+++ codes/src/app/api/auth/getUserData/route.ts
@@ -4,17 +4,22 @@ import { validateJWT } from '@/helpers/validateJWT';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest, response: NextResponse){
+  console.log("AAAAAAAAAAAAAAAA sono in getUserData");
   try {
     await connect();
     const userId = await validateJWT(request);
     if (!userId) {
       return NextResponse.json({ message: 'Invalid or missing token' }, { status: 401});
     }
+    console.log("AAAAAAAAAAAAAAAA userId=" + userId);
+
     const user = await User.findById(userId).select("-password");
 
     if (!user) {
       return NextResponse.json({ message: 'User not found' }, { status: 404});
     }
+    
+    console.log("AAAAAAAAAAAAAAAA userId=" + user);
 
     return NextResponse.json({ data: user}, { status: 200 });
   } catch (error: any) {
