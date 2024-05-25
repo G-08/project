32f0e0bca41535f72912dd03fea50a66b2c65ada
EnigmaDesign <luca.buonocore@studenti.unitn.it>
2024-05-25 11:01:06 +0200

    messo il pulsante di theme switche nel profilo dell'utente

--- codes/src/app/profilo/page.tsx
+++ codes/src/app/profilo/page.tsx
@@ -1,10 +1,27 @@
 'use client'
+
 import { Button, Form, message } from 'antd'
 import axios from 'axios';
 import { useRouter } from 'next/navigation'
 import Sidebar from '@/components/sidebar';
 import { useEffect, useState } from 'react';
 import UpdateForm from '@/components/updateForm';
+
+
+const [theme, setTheme] = useState("Light");
+
+
+useEffect(()=> {
+  if(theme === "dark") {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+}, [theme]); 
+
+const handleThemeSwitch = () => {
+  setTheme(theme === "dark" ? "light" : "dark");
+};
 
 const Profilo = () => {
   
@@ -25,8 +42,9 @@ const Profilo = () => {
 
   return (
     <>
-      <div className='flex'>
+      <div className='flex dark:bg-black dark:text-white'>
         <Sidebar></Sidebar>
+        <button onClick={handleThemeSwitch}>Switch Mode</button>
         <h1>Profilo</h1>
         <UpdateForm></UpdateForm>      
       </div>
